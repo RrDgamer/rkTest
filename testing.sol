07bc61dff991da125dde56b5c0d45f9d1e2269d8
RrDgamer <136418956+RrDgamer@users.noreply.github.com>
2023-12-05 21:00:33 +0000

    Create testing.sol

---
+++ testing.sol
@@ -0,0 +1,31 @@
+/*
+* Copyright © 2020 reflect.finance. ALL RIGHTS RESERVED.
+ */
+
+pragma solidity ^0.6.2;
+
+import "openzeppelin-solidity/contracts/GSN/Context.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/utils/Address.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+contract REFLECT is Context, IERC20, Ownable {
+    using SafeMath for uint256;
+    using Address for address;
+
+    mapping (address => uint256) private _rOwned;
+    mapping (address => uint256) private _tOwned;
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    mapping (address => bool) private _isExcluded;
+    address[] private _excluded;
+
+ uint256 private constant MAX = ~uint256(0);
+    uint256 private constant _tTotal = 10 * 10**6 * 10**9;
+    uint256 private _rTotal = (MAX - (MAX % _tTotal));
+    uint256 private _tFeeTotal;
+
+    string private _name = 'reflect.finance';
+    string private _symbol = 'RFI';
+    uint8 private _decimals = 9;
